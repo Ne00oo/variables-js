38ecba048be9d4e083c91356290d40ce119bf387
Yivan2021 <manuel.rodriguez@softwareestrategico.com>
2022-03-25 09:41:05 -0500

    introduccion-html

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
-// function mayorNumero (a, b) {
-//     if (typeof(a) === 'number') {
-//         console.log('paso validaciones para a');
-//     }else{
-//         return 'a, no es un numero';
-//     }
+// function mayorNumero (dato1, dato2) {
+//     console.log(dato1);
+//     console.log(typeof(dato1));
+//     console.log(typeof(dato1) !== 'number');
 
-//     if (typeof(b) === 'number') {
-//         console.log('si es numero', b);
-//     }else{
-//         return 'b, no es un numero';
+//     if (typeof(dato1) !== 'number') {
+
+//         return 'a, no es un numero';
+
 //     }
+//     console.log(dato2);
+//     console.log(typeof(dato2));
+//     console.log(typeof(dato2) !== 'number');    
+
+//     if (typeof(dato2) !== 'number') {
+
+//         return 'b, no es un numero';
+
+//     }
+    
+//     return 'paso validaciones';
   
 // }
+// const resultado = mayorNumero(100, 8, 9);
+// console.log('====================================');
+// console.log(resultado);
+// console.log('====================================');
 
+function detector(respuesta) {
+    const resp =  respuesta=="si" ? ('irás a la cárcel') :( 'irás a casa');
 
-function mayorNumero (dato1, dato2) {
-    console.log(dato1);
-    console.log(typeof(dato1));
-    console.log(typeof(dato1) !== 'number');
+    console.log(resp);
+	// if(respuesta=="si"){
+	// 	alert("irás a la cárcel");
+	// }else if(respuesta=="no"){
+	// 	alert("irás a casa");
+	// }else{
+	// 	alert("la documentación por favor");
+	// }
 
-    if (typeof(dato1) !== 'number') {
-
-        return 'a, no es un numero';
-
-    }
-    console.log(dato2);
-    console.log(typeof(dato2));
-    console.log(typeof(dato2) !== 'number');    
-
-    if (typeof(dato2) !== 'number') {
-
-        return 'b, no es un numero';
-
-    }
-    
-    return 'paso validaciones';
-  
 }
-const resultado = mayorNumero(100, 8, 9);
-console.log('====================================');
-console.log(resultado);
-console.log('====================================');
-
+// detector('si');
+detector('si');
